606adc723529fb72b290739f56efe5d58fa6e467
Kolibroid <152735803+Kolibroid@users.noreply.github.com>
2023-12-03 22:12:41 +0000

    Day 2

---
+++ src/main/kotlin/Y2023/day2.kt
@@ -0,0 +1,55 @@
+package Y2023
+
+import Day
+
+
+// Convert a line to a map of colour: max count
+fun maximumCounts(game: String): Map<String, Int> {
+    val (_, pulls) = game.split(": ")
+
+    val colourMax = mutableMapOf(
+        "red" to 0,
+        "green" to 0,
+        "blue" to 0,
+    )
+
+    pulls.split("(,|;) ".toRegex()).forEach {
+        val (num, col) = it.split(" ")
+        colourMax[col] = maxOf(num.toInt(), colourMax[col]!!)
+    }
+
+    return colourMax
+}
+
+
+val day2 = Day(2023, 2) {
+    val input by fetchInput()
+
+
+    // === Part 1 === //
+    puzzle {
+        input.lines()
+            .filter(String::isNotEmpty)
+            // Convert each line to a map of colour: max count
+            .map(::maximumCounts)
+            .withIndex()
+            .filter {
+                it.value["red"]!! <= 12
+                && it.value["green"]!! <= 13
+                && it.value["blue"]!! <= 14
+            }
+            .sumOf {
+                    it.index+1
+            }.toString()
+    }
+
+    // === Part 2 === //
+    puzzle {
+        input.lines()
+            .filter(String::isNotEmpty)
+            .map(::maximumCounts)
+            .sumOf {
+                it.values.fold(1, Int::times)
+            }.toString()
+    }
+}
--- src/main/kotlin/Y2023/year.kt
+++ src/main/kotlin/Y2023/year.kt
@@ -1,5 +1,6 @@
 package Y2023
 
 val Y2023 = listOf(
-    day1
+    day1,
+    day2,
 )
